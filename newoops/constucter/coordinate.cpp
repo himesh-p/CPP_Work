4b2a0299be9bd18d9cff72cc74ef679d42749542
Himesh <himeshpatel0704@gmail.com>
2024-03-20 09:06:09 +0530

    constucter

--- newoops/constucter/coordinate.cpp
+++ newoops/constucter/coordinate.cpp
@@ -6,24 +6,40 @@ class Coordinate{
     int x;
     int y;
 
-    Coordinate(){
-        x = 0;
-        y = 0;
-    }
-    void scan(){
-        cout << "Enter the first coordinate:- " << endl;
-        cin >> x;
-        cout << "Enter the second coordinate:- " << endl;
-        cin >> y;
+    Coordinate(int x1, int y1){
+        x = x1;
+        y = y1;
     }
     void display(){
         cout << "The coordinate is :- " << endl;
-        cout << "(" << x << ","<< y << ")";
+        cout << "(" << x << ","<< y << ")" << endl;
+    }
+    void qudrant(){
+        if(x>0 && y>0){
+            cout << "The coordinate is in first qudrant" << endl;
+        }
+        else if(x<0 && y>0){
+            cout << "The coordinate is in second qudrant" << endl;
+        }
+        else if(x<0 && y<0){
+            cout << "The coordinate is in third qudrant" << endl;
+        }
+        else if(x>0 && y<0){
+            cout << "The coordinate is in fourth qudrant" << endl;
+        }
+        else{
+            cout << "The coordinate is in origin." << endl;
+        }
     }
 };
 
 int main(){
-    Coordinate c;
-    c.scan();
+    int x1,y1;
+    cout << "Enter the first coordinate:- " << endl;
+        cin >> x1;
+        cout << "Enter the second coordinate:- " << endl;
+        cin >> y1;
+    Coordinate c(x1,y1);
     c.display();
+    c.qudrant();
 }
