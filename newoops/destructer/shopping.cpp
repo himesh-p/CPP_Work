6828f841c8bc3fd52d41b1a60a6620dc17d983b0
Himesh <himeshpatel0704@gmail.com>
2024-04-09 17:12:39 +0530

    destructer

---
+++ newoops/destructer/shopping.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class ShoppingCart{
+    private:
+    string name;
+    float price;
+
+    public:
+    ShoppingCart(string n , float p){
+        name = n;
+        price = p;
+        cout << "Constructer called....." << endl;
+    }
+    void display(){
+        cout << "The name of Product:- " << name << endl;
+        cout << "The price of Product:- " << price << endl;
+    }
+    ~ShoppingCart(){
+        cout << "Destructor called......" << endl;
+        cout << "The details of this Products name:- " << name << " , price:- " << price << " are destroyed." << endl;
+    }
+};
+
+int main(){
+    ShoppingCart obj("Jeans" , 2000);
+    cout << "The Shopping Products details before distruction:- " << endl;
+    obj.display();
+    return 0;
+}
