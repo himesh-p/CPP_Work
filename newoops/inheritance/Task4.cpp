bc0409cea55e0a804c7f2ffc168f66bfdbca6cbf
Himesh <himeshpatel0704@gmail.com>
2024-04-16 08:53:25 +0530

    hh

--- newoops/inheritance/Task4.cpp
+++ newoops/inheritance/Task4.cpp
@@ -1,54 +1,54 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-class Vehicle {
-public:
-    void display() {
-        cout << "Vehicle have two type:- " << endl;
-    }
-};
+// class Vehicle {
+// public:
+//     void display() {
+//         cout << "Vehicle have two type:- " << endl;
+//     }
+// };
 
-class Engine {
-public:
-    void display() {
-        cout << "Engine have two type:- " << endl;
-    }
-};
+// class Engine {
+// public:
+//     void display() {
+//         cout << "Engine have two type:- " << endl;
+//     }
+// };
 
-class Car : public Vehicle {
-public:
-    void display() {
-        cout << "Car is the part of Vehical." << endl;
-    }
-};
+// class Car : public Vehicle {
+// public:
+//     void display() {
+//         cout << "Car is the part of Vehical." << endl;
+//     }
+// };
 
-class Motorcycle : public Vehicle {
-public:
-    void display() {
-        cout << "Motorcycle is the part of Vehical." << endl;
-    }
-};
+// class Motorcycle : public Vehicle {
+// public:
+//     void display() {
+//         cout << "Motorcycle is the part of Vehical." << endl;
+//     }
+// };
 
-class ElectricEngine : public Engine {
-public:
-    void display() {
-        cout << "Electric Engine is part of Engine" << endl;
-    }
-};
+// class ElectricEngine : public Engine {
+// public:
+//     void display() {
+//         cout << "Electric Engine is part of Engine" << endl;
+//     }
+// };
 
-class GasEngine : public Engine {
-public:
-    void display() {
-        cout << "Gas Engine is the part of Engine" << endl;
-    }
-};
+// class GasEngine : public Engine {
+// public:
+//     void display() {
+//         cout << "Gas Engine is the part of Engine" << endl;
+//     }
+// };
 
 
-int main() {
-    Vehicle obj;    
-    obj.display();
-    obj.display();  //Ambiguity: which display() to call?
-    obj.GasEngine::display();
+// int main() {
+//     Car C;    
+//     C.display();
+//     C.Vehicle::display();  //Ambiguity: which display() to call?
+//     // Motorcycle::display();
 
-    return 0;
-}
+//     return 0;
+// }
