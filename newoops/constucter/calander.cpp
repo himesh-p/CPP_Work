67eeb861793c21258335c4cb2c0c3736d8756472
Himesh <himeshpatel0704@gmail.com>
2024-04-21 14:39:44 +0530

    constructer

---
+++ newoops/constucter/calander.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Calander{
+private:
+    string name;
+    string date;
+    string time;
+public:
+    Calander(string n, string d, string i){
+        name = n;
+        date = d;
+        time = i;
+    }
+    void display(){
+        cout << endl << "The name of this event is:- " << name << endl;
+        cout << "The date of this event is:- " << date << endl;
+        cout << "The time of this event is:- " << time << endl;
+    }
+};
+int main(){
+
+    Calander obj("New Year Party" , "31st December 2023", "8.00 PM");
+    obj.display();
+    return 0;
+}
