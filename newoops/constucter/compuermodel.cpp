da2da03ca3aa49962dead6b8abd31a89867a64af
Himesh <himeshpatel0704@gmail.com>
2024-04-21 14:31:34 +0530

    contructer

---
+++ newoops/constucter/compuermodel.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Computer{
+private:
+    string brand;
+    string model;
+    float price;
+public:
+    Computer(string b, string m, float p){
+        brand = b;
+        model = m;
+        price = p;
+    }
+    void display(){
+        cout << endl << "The Brand of this computer is:- " << brand << endl ;
+        cout << "The Model of this computer is:- " << model << endl ;
+        cout << "The Price of this computer is:- " << price << endl ;
+    }
+};
+int main(){
+
+    Computer c("Dell","S2722QC", 39999);
+    c.display();
+    return 0;
+}
